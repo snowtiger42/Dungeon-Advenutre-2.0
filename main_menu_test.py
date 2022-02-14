acae915a36082b90fa4c88d72e40249094ea0f94
Procrastinav <94326046+Procrastinav@users.noreply.github.com>
2022-02-13 23:45:05 -0500

    Add files via upload

---
+++ main_menu_test.py
@@ -0,0 +1,21 @@
+import unittest
+import random
+from mock_game import MockGame as Game
+
+
+class MainMenuTest(unittest.TestCase):
+    def test_play_game(self):
+        pass
+
+    def test_instructions(self):
+        pass
+
+    def test_exit_game(self):
+        pass
+
+    def test_load_game(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
