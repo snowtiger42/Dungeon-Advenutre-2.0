2d1b0c9d5ccc0f51216dca8c0496092690ba62fa
Kevin <92612487+snowtiger42@users.noreply.github.com>
2022-02-15 13:32:59 -0800

    Update dungeonCharacter.py

--- dungeonCharacter.py
+++ dungeonCharacter.py
@@ -2,7 +2,23 @@ from abc import ABCMeta, abstractmethod
 from abc import abstractmethod
 
 
-class DungeonCharacter(metaclass=ABCMeta):
+class DungeonCharacter(object, metaclass=ABCMeta):
+
+    @staticmethod
+    @abstractmethod
+    def __init__(self, name, game, max_hp, chance_to_dodge, chance_to_hit):
+        if self.__class__ == DungeonCharacter:
+            raise Exception('I am abstract!')
+        self.__name = name
+        self.__game = game
+        self.__max_hp = max_hp
+        self.__chance_to_dodge = chance_to_dodge
+        self.__chance_to_hit = chance_to_hit
+
+    @staticmethod
+    @abstractmethod
+    def __str__():
+        pass
 
     @staticmethod
     @abstractmethod
@@ -11,22 +27,63 @@ class DungeonCharacter(metaclass=ABCMeta):
 
     @staticmethod
     @abstractmethod
-    def damage_range(self):
+    def set_name(self):
         pass
 
     @staticmethod
     @abstractmethod
-    def attack_speed(self):
+    def get_hp(self):
         pass
 
     @staticmethod
     @abstractmethod
-    def hit_chance(self):
+    def set_hp(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_attack_damage_range(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def set_attack_damage_range(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_attack_speed(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def set_attack_speed(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_chance_to_hit(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def set_chance_to_hit(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_chance_to_dodge(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def set_chance_to_dodge(self):
         pass
 
     @staticmethod
     @abstractmethod
     def is_dead(self):
         pass
+
 
 
