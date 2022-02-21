3684dac56f70d111eb42ed1c01fc765b550ffe09
Kevin <92612487+snowtiger42@users.noreply.github.com>
2022-02-20 16:25:34 -0800

    Update phonenix.py

--- phonenix.py
+++ phonenix.py
@@ -1,23 +1,36 @@
-
 from abc import ABCMeta, abstractmethod
+import random
 from monster import Monster
-from random import randrange
+
 
 class Phoenix(Monster):
+    def __init__(self, diff, name):
+        super().__init__(name, 250, 333, range(250, 333), range(250, 333), 33, 44, range(33, 44), 3, .60,
+                         .75, random.uniform(.60, .75), .3, .5, random.uniform(.3, .5), .30, .5, 20)
+        self.__diff = diff
+        self.__name = name
+
+    def get_diff(self):
+        self.__diff.get()
+
     # overriding 'name' method
     def get_name(self):
-        return 'Phoenix'
-    # overriding 'damage_range' method
-    def damage_range(self, count):
-        return range(0, count)
-    # overriding 'attack_speed' method
-    def attack_speed(self):
-        return randrange(0, 1000) # return whatever the speed is
-    # overriding 'hit_chance' method
-    def hit_chance(self):
-        return randrange(0, 100) # return % hit chance
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = "Phoenix"
+
     # overriding 'is_dead' method
     def is_dead(self):
-        monster_hp = randrange(0, 1000)
-        if (monster_hp <= 0): return True
-        else: return False
+        if self.__current_hp <= 0:
+            return self.__current_hp <= 0
+        else:
+            return False
+
+    # def __str__(self):
+    #     prefix = super().__str__()
+    #     p = format(prefix)
+    #     return print(p)
+
+p = Phoenix(1, "Phoenix")
+print(p)
