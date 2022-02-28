99365cbb54a6bbea9d7615acbc5d27368fd98da2
Kevin <92612487+snowtiger42@users.noreply.github.com>
2022-02-27 22:19:43 -0800

    I've updated your dungeon character.
    Some issues to discuss with Gary. As it's written, only the character with the greatest speed attacks (always). second, in the test bellow, the damage of get_attack_range is a constant value when it should be a range of values (i.e. between 40 to 80; instead of 70 constantly). also, please ask how to apply this to the hero and monster child classes and how to implement special skills.

--- dungeonCharacter.py
+++ dungeonCharacter.py
@@ -3,15 +3,14 @@ from abc import abstractmethod
 from mock_game import MockGame as Game
 import sys
 import random
+# from combatMode import CombatMode
 import numpy as np
 
 
 class DungeonCharacter(object, metaclass=ABCMeta):
-    # class DungeonCharacter:
 
-    def __init__(self, name, min_hp, max_hp, generated_hp, current_hp, attack_min, attack_max, attack_damage_range
-                 , attack_speed, chance_to_hit_min, chance_to_hit_max, chance_to_hit, chance_to_dodge_min,
-                 chance_to_dodge_max, chance_to_dodge):
+    def __init__(self, name, min_hp, max_hp, attack_min, attack_max, attack_speed, chance_to_hit_min, chance_to_hit_max,
+                 chance_to_dodge_min, chance_to_dodge_max):
         # if self.__class__ == DungeonCharacter:
         #     raise Exception('I am abstract!')
         self.__name = name
@@ -21,119 +20,102 @@ class DungeonCharacter(object, metaclass=ABCMeta):
 
         self.__generated_hp = random.randrange(self.__min_hp, self.__max_hp)
         self.__current_hp = self.__generated_hp
-        # self.__generated_hp = generated_hp
-        # self.__current_hp = current_hp
         self.__attack_min = attack_min
         self.__attack_max = attack_max
 
         self.__attack_damage_range = random.randrange(self.__attack_min, self.__attack_max)
-        # self.__attack_damage_range = attack_damage_range
         self.__attack_speed = attack_speed
         self.__chance_to_dodge_min = chance_to_dodge_min
         self.__chance_to_dodge_max = chance_to_dodge_max
-        self.__chance_to_dodge = chance_to_dodge
+        self.__chance_to_dodge = random.uniform(self.__chance_to_dodge_min, self.__chance_to_dodge_max)
         self.__chance_to_hit_min = chance_to_hit_min
         self.__chance_to_hit_max = chance_to_hit_max
-        self.__chance_to_hit = chance_to_hit
+        # self.__chance_to_hit = chance_to_hit
+        self.__chance_to_hit = random.uniform(self.__chance_to_hit_min, self.__chance_to_hit_max)
 
     def get_min_hp(self):
         return self.__min_hp
 
-    def set_min_hp(self):
-        self.__min_hp = 100
+    def set_min_hp(self, min_hp):
+        self.__min_hp = min_hp
 
     def get_max_hp(self):
         return self.__max_hp
 
-    def set_max_hp(self):
-        self.__max_hp = 200
+    def set_max_hp(self, max_hp):
+        self.__max_hp = max_hp
 
-    # def get_generated_hp(self):
-    #     return self.__generated_hp
-    #
-    # def set_generated_hp(self):
-    #     self.__generated_hp = range(int(self.__min_hp), int(self.__max_hp))
-    #
+    def get_generated_hp(self):
+        return self.__generated_hp
+
+    def set_generated_hp(self, hp):
+        self.__generated_hp = hp
+
     def get_current_hp(self):
         return self.__current_hp
 
-    def set_current_hp(self):
-        self.__current_hp = self.__generated_hp
+    def set_current_hp(self, new_hp):
+        self.__current_hp = new_hp
 
     def get_attack_min(self):
         return self.__attack_min
 
-    def set_attack_min(self):
-        self.__attack_min = 30
+    def set_attack_min(self, attack_min):
+        self.__attack_min = attack_min
 
     def get_attack_max(self):
         return self.__attack_max
 
-    def set_attack_max(self):
-        self.__attack_max = 80
+    def set_attack_max(self, attack_max):
+        self.__attack_max = attack_max
 
     def get_attack_damage_range(self):
         return self.__attack_damage_range
 
-    def set_attack_damage_range(self):
-        self.__attack_damage_range = random.uniform(self.__attack_min, self.__attack_max)
+    def set_attack_damage_range(self, damage_rate):
+        self.__attack_damage_range = damage_rate
 
     def get_attack_speed(self):
         return self.__attack_speed
 
-    def set_attack_speed(self, new_speed):
-        self.__attack_speed = new_speed
+    def set_attack_speed(self, speed):
+        self.__attack_speed = speed
 
     def get_chance_to_hit_min(self):
         return self.__chance_to_hit_min
 
-    def set_chance_to_hit_min(self, new_hit_min):
-        self.__chance_to_hit_min = new_hit_min
+    def set_chance_to_hit_min(self, hit_min):
+        self.__chance_to_hit_min = hit_min
 
     def get_chance_to_hit_max(self):
         return self.__chance_to_hit_max
 
-    def set_chance_to_hit_max(self, new_hit_max):
-        self.__chance_to_hit_max = new_hit_max
+    def set_chance_to_hit_max(self, hit_max):
+        self.__chance_to_hit_max = hit_max
 
     def get_chance_to_hit(self):
         return self.__chance_to_hit
 
-    def set_chance_to_hit(self):
-        self.__chance_to_hit = random.uniform(self.__chance_to_hit_min, self.__chance_to_hit_max)
+    def set_chance_to_hit(self, hit_chance):
+        self.__chance_to_hit = hit_chance
 
     def get_chance_to_dodge_max(self):
         return self.__chance_to_dodge_max
 
-    def set_chance_to_dodge_max(self, new_chance_to_dodge_min):
-        self.__chance_to_dodge_max = new_chance_to_dodge_min
+    def set_chance_to_dodge_max(self, dodge_max):
+        self.__chance_to_dodge_max = dodge_max
 
     def get_chance_to_dodge_min(self):
         return self.__chance_to_dodge_min
 
-    def set_chance_to_dodge_min(self, new_chance_to_dodge_min):
-        self.__chance_to_dodge_min = new_chance_to_dodge_min
+    def set_chance_to_dodge_min(self, dodge_min):
+        self.__chance_to_dodge_min = dodge_min
 
     def get_chance_to_dodge(self):
         return self.__chance_to_dodge
 
-    def set_chance_to_dodge(self):
-        self.__chance_to_dodge = random.uniform(self.__chance_to_dodge_min, self.__chance_to_dodge_max)
-
-    def rebirth(self):
-        self.__current_hp = self.get_max_hp()
-
-    def heal(self):
-        heal = random.randrange(25, 50)
-
-        self.__current_hp += heal
-
-        if self.__current_hp >= self.__generated_hp:
-            self.__current_hp = self.__generated_hp
-
-    # @abstractmethod
-    # def __str__():
-    #     pass
+    def set_chance_to_dodge(self, chance_to_dodge):
+        self.__chance_to_dodge = chance_to_dodge
 
     def get_name(self):
         """
@@ -148,26 +130,54 @@ class DungeonCharacter(object, metaclass=ABCMeta):
         """
         Returns true if the adventurer's HP is above 0, and False otherwise.
         """
-        return (self.__current_hp <= 0), sys.exit()  # display Game Over message, sys.exit() in main code
+        print("\nOh NO!!! You Died")
+        print(self.__str__())
+        return self.get_current_hp(), sys.exit()
+        # return (self.set_current_hp(self.get_current_hp())), sys.exit()
 
-    # determines whether an attack is a hit or a miss. Returns true if attack is successful.
-    # Generates random number. Compares random number to attack chance.
-   
-    def attack(self, attacker, defender):
+    def attack(self):
+        pass
 
-        while defender.get_current_hp() > 0 and attacker.get_current_hp() > 0:
+    def take_damage(self, damage, source):
+        """
+        Reduces HP by the indicated amount and makes an announcement.
+        """
+        # damage = self.__attack_damage_range
+        self.__current_hp -= damage
+
+        if self.__current_hp <= 0:
+            self.set_current_hp(0)
+            self.is_dead()
+
+        print(f"Oh no! {self.__name} took {damage} dmg from {source}!\nThey are now at {self.__current_hp} hp!")
+        # self.__game.announce(f"Oh no! {self.__name} took {damage} dmg from {source}!\nThey are now at {self.__current_hp} hp!")
+
+
+        # determines whether an attack is a hit or a miss. Returns true if attack is successful.
+        # Generates random number. Compares random number to attack chance.
+    def combat(attacker, defender):
+
+        while defender.get_current_hp() > 0 or attacker.get_current_hp() > 0:
+            attacker_damage = attacker.get_attack_damage_range()
+            defender_new_hp = defender.get_current_hp()
+
+            defender_damage = defender.get_attack_damage_range()
+            attacker_new_hp = attacker.get_current_hp()
+
+            attcker_result = defender_new_hp - attacker_damage
+            defender_result = attacker_new_hp - defender_damage
 
             dodge_chance = random.uniform(.1, 1)
-
             # character will attack another character
-            hit_chance = random.uniform(.1, 1)  # generates a random % chance of a successful attack by this character
-            if attacker.get_attack_speed >= defender.get_attack_speed:
+            hit_chance = random.uniform(.1,
+                                        1)  # generates a random % chance of a successful attack by this character
+            if attacker.get_attack_speed() >= defender.get_attack_speed():
 
-                if attacker.get_chance_to_hit >= hit_chance:
-                    if defender.get_chance_to_dodge < dodge_chance:
-                        attacker.get_attack_damage_range() - defender.get_current_hp()
-                        print(f"The {attacker} has dealt the {defender} {attacker.get_damage_range()} damage to their hp."
-                              f"The {defender} has {defender.get_current_hp()} hp.")
+                if attacker.get_chance_to_hit() >= hit_chance:
+                    if defender.get_chance_to_dodge() < dodge_chance:
+                        defender.set_current_hp(attcker_result)
+                        print(f"The {attacker} has dealt the {defender} {attacker_damage} damage to "
+                            f"their hp. The {defender} has {defender_new_hp} hp.")
                     else:
                         print(f"The {attacker} has missed resulting in {0} damage to {defender} hp."
                               f"The {defender} has {defender.get_current_hp()} hp.")
@@ -177,8 +187,8 @@ class DungeonCharacter(object, metaclass=ABCMeta):
 
             elif attacker.get_attack_speed < defender.get_attack_speed:
                 if defender.get_chance_to_hit >= hit_chance:
-                    if attacker.get_chance_to_dodge < dodge_chance:
-                        defender.get_attack_damage_range() - attacker.get_current_hp()
+                    if attacker.get_chance_to_dodge() < dodge_chance:
+                        attacker.set_current_hp(defender_result)
                         print(f"The {defender} has dealt the {attacker} {defender.get_damage_range()} damage to"
                               f" their hp. The {attacker} has {attacker.get_current_hp()} hp.")
                     else:
@@ -189,22 +199,14 @@ class DungeonCharacter(object, metaclass=ABCMeta):
                           f"The {attacker} has {attacker.get_current_hp()} hp.")
 
             if attacker.get_current_hp() <= 0:
-                print(f"The {attacker} hp is {attacker.get_current_hp()}/{attacker.get_generated_hp}. You have died.")
+                print(
+                    f"The {attacker} hp is {attacker.get_current_hp()}/{attacker.get_generated_hp}. You have died.")
                 attacker.is_dead()
             elif defender.get_current_hp() <= 0:
-                print(f"The {defender} hp is {defender.get_current_hp()}/{defender.get_generated_hp}. The {defender}"
-                      f" has died.")
-
-
-
-    def take_damage(self, damage, source):
-        """
-        Reduces HP by the indicated amount and makes an announcement.
-        """
-        self.__current_hp -= damage
-        print(f"Oh no! {self.__name} took {damage} dmg from {source}!\nThey are now at {self.__current_hp} hp!")
-
-        # self.__game.announce(f"Oh no! {self.__name} took {damage} dmg from {source}!\nThey are now at {self.__current_hp} hp!")
+                print(
+                    f"The {defender} hp is {defender.get_current_hp()}/{defender.get_generated_hp}. The {defender}"
+                    f" has died.")
+                break
 
     # @abstractmethod
     def __str__(self):
@@ -220,12 +222,39 @@ class DungeonCharacter(object, metaclass=ABCMeta):
         dodge_str = f"Dodge Chance: {round(self.__chance_to_dodge_min * 100)}% to {round(self.__chance_to_dodge_max * 100)}% "
         accuracy_str = f"Hit Chance: {round(self.__chance_to_hit_min * 100)}% to {round(self.__chance_to_hit_max * 100)}%"
 
-        return name_str, hp_str, attack_str, speed_str, dodge_str, accuracy_str
-
+        # return name_str, hp_str, attack_str, speed_str, dodge_str, accuracy_str
         # return status_items
 
+        status_items = [name_str, hp_str, attack_str, speed_str, dodge_str, accuracy_str]
 
-# a = DungeonCharacter("Kevin", 100, 200, range(100, 200), range(100, 200), 30, 80, range(30, 80), 4, .60, .75,
-#                      random.uniform(.60, .75), .20, .30, random.uniform(.20, .30))
-#
-# print(a)
+        line_size = 0
+        for line in status_items:
+            if len(line) > line_size:
+                line_size = len(line)
+
+        # create borders
+        border = "+" + "-" * (line_size + 2) + "+"
+
+        # add spacers to all status items based on max length
+        # so that right border is even
+        output_str = "\n" + border
+        for line in status_items:
+            output_str += f"\n| {line}"
+            white_space = line_size - len(line)
+            if white_space > 0:
+                output_str += " " * white_space
+            output_str += " |"
+
+        output_str += f"\n{border}\n"
+        return output_str
+
+
+kevin = DungeonCharacter("Kevin", 200, 300, 40, 80, 4, .70, .85, .30, .40)
+talia = DungeonCharacter("Talia", 100, 200, 30, 80, 3, .60, .75, .20, .30)
+
+
+print(kevin)
+print(talia)
+DungeonCharacter.combat(kevin, talia)
+
+# CombatMode()
