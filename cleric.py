190f72840f464c9f3c780d5641a9a50a3dc7392f
Kevin <92612487+snowtiger42@users.noreply.github.com>
2022-03-06 21:07:37 -0800

    Update cleric.py

--- cleric.py
+++ cleric.py
@@ -8,9 +8,9 @@ class Cleric(Hero):
     A class the handles information for the Cleric
     """
 
-    def __init__(self, name):
-        super().__init__(name, 100, 180, 30, 60, 5, .75, .80, .4, .5, .30, .4)
-        self.__name = name
+    def __init__(self, name, game):
+        super().__init__(name, game, 100, 175, 30, 60, 5, .75, .80, .4, .5, .30, .4)
+        # self.__name = name
 
     def special_move(self):
         reduced_chance_to_hit = (self.get_chance_to_hit()) / 2
@@ -23,32 +23,32 @@ class Cleric(Hero):
                 self.set_current_hp(self.get_generated_hp())
             else:
                 self.set_current_hp(new_hp)
-            print(f"You used the Heal ability! It heals {heal} HP, bringing you to {self.get_current_hp()}.")
+            self.__game.announce(f"You used the Heal ability! It heals {heal} HP, bringing you to {self.get_current_hp()}.")
             # self.__game.announce(f"Used a health potion! It heals {heal} HP, bringing you to {self.__current_hp}.")
             return True
         else:
-            print(f"You tried to used the Heal ability, but it failed to work! It heals {0} HP, bringing you to "
+            self.__game.announce(f"You tried to used the Heal ability, but it failed to work! It heals {0} HP, bringing you to "
                   f"{self.get_current_hp()}.")
             return False
 
 
-adventurer = Cleric("Talia")
-print(adventurer)
-
-
-print("\n------------------------print adventurer status ('empty', try using either potion)-------------------------")
-adventurer.use_health_potion()
-adventurer.use_vision_potion()
-
-adventurer.take_damage(99, "Phoenix")
-
-print(adventurer)
-
-adventurer.special_move()
-adventurer.special_move()
-adventurer.special_move()
-
-
-print(adventurer)
+# adventurer = Cleric("Talia")
+# print(adventurer)
+#
+#
+# print("\n------------------------print adventurer status ('empty', try using either potion)-------------------------")
+# adventurer.use_health_potion()
+# adventurer.use_vision_potion()
+#
+# adventurer.take_damage(99, "Phoenix")
+#
+# print(adventurer)
+#
+# adventurer.special_move()
+# adventurer.special_move()
+# adventurer.special_move()
+#
+#
+# print(adventurer)
 
 
