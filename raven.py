d6d097956e35c0a1b051a271f910ddbff8ad7fff
Kevin <92612487+snowtiger42@users.noreply.github.com>
2022-03-15 14:39:08 -0700

    Update raven.py

--- raven.py
+++ raven.py
@@ -3,6 +3,8 @@ import random
 
 from dungeonCharacter import DungeonCharacter
 from monster import Monster
+from mock_game import MockGame as Game
+
 
 """
 self, name, min_hp, max_hp, generated_hp, current_hp, attack_min, attack_max, attack_damage_range,
@@ -10,17 +12,33 @@ self, name, min_hp, max_hp, generated_hp, current_hp, attack_min, attack_max, at
                  chance_to_dodge_max, chance_to_dodge, chance_to_regenerate_min, chance_to_regenerate_max,
                  regenerate_amount
 """
-
-
 class Raven(Monster):
-    def __init__(self, diff, game):
-        super().__init__("Raven", game, 100, 120, 40, 60, 1, .40, .60, .10, .25, .10, .20, 20)
+    def __init__(self, diff, name, game):
+        super().__init__(name, game, 100, 120, 40, 60, 1, .40, .60, .10, .25, .10, .20, 20)
+        self.__game = game
         self.__diff = diff
+        self.__name = name
 
     def get_diff(self):
         self.__diff.get()
 
+    def get_name(self):
+        return self.__name
 
+    def set_name(self, name):
+        self.__name = "Raven"
+
+# jack = Raven(1, "Raven", Game())
+# jill = Raven(1, "Raven", Game())
+# jack.fight(jack, jill)
+
+
+    # def __str__(self):
+    #     self.__game.announcement(f"{super().__str__()}")
+
+
+# a = Raven(1, "Raven", Game())
+# print(a)
 # if __name__ == "__main__":
 #     test = Raven(10)
 #     test.take_damage(500, test)
